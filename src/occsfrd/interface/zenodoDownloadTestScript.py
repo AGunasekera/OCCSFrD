ed48a795ecb8df6b0025d8308bcfe6c2d8829160
Alexander Gunasekera <alexander.gunasekera@gmail.com>
2026-03-16 14:53:28 +0000

    zenodo download works

--- src/occsfrd/interface/zenodoDownloadTestScript.py
+++ src/occsfrd/interface/zenodoDownloadTestScript.py
@@ -6,14 +6,18 @@ assert (len(sys.argv) == 2) and isinstance(sys.argv[1], str)
 ACCESS_TOKEN = sys.argv[1]
 headers = {'Authorization': f'Bearer {ACCESS_TOKEN}'}
 deposition_url = 'https://sandbox.zenodo.org/api/deposit/depositions'
-which_deposition = 1
+which_deposition = 0
 r = requests.get(deposition_url, headers=headers)
-r = requests.get(deposition_url + "/" + r.json()[which_deposition]['id'], headers=headers)
+r = requests.get("%s/%s" % (deposition_url, r.json()[which_deposition]['id']), headers=headers)
 fNamesAndDownloadLinks = [(file['filename'], file['links']['download']) for file in r.json()['files']]
+
+eqnFileName = "N0S0_lCCSD.eqn"
+
 for name, link in fNamesAndDownloadLinks:
-    with open(name, 'wb') as f:
-        r = requests.get(link, headers=headers)
-        f.write(r.content)
+    if name == eqnFileName:
+        with open(name, 'wb') as f:
+            r = requests.get(link, headers=headers)
+            f.write(r.content)
 # r = requests.get(f0download, headers=headers)
 # with open(f0name, 'wb') as f0:
 #     f0.write(r.content)
