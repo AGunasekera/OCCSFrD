058b6be4f0e2fbcbf34f07cd2eeac0d9a7fc1626
Alexander Gunasekera <alexander.gunasekera@gmail.com>
2026-03-20 16:18:53 +0000

    meson build

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from setuptools import setup, Extension
 
-setup()
+setup(name='occsfrd',
+      ext_modules = [Extension("occsfrd.wick.contract", sources=["src/occsfrd/wick/contract/contract.f90"], extra_objects=["src/occsfrd/wick/contract/contractEXT.so"])]
+      )
 # setup(
 #     name='mypackage',
 #     version='0.0.1',
